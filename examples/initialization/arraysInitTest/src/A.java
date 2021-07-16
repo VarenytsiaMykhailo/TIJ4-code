edf13859d9c1de0720ffb8163b4974bbf4ca8c5c
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2021-07-16 17:23:22 +0300

    1

---
+++ examples/initialization/arraysInitTest/src/A.java
@@ -0,0 +1,6 @@
+public class A {
+
+    public A(String str) {
+        System.out.println(str);
+    }
+}
---
+++ examples/initialization/arraysInitTest/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = new String[22];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = "str" + i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        A[] arr2 = new A[22];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = new A(i + "");
+        }
+    }
+}
