366cfe9aa88cb7c682aaa36fb1d62b537af88f16
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2021-07-17 04:00:34 +0300

    1

---
+++ examples/access/packageTest/src/Main.java
@@ -0,0 +1,10 @@
+import com.github.varenytsiamykhailo.packageTest.*;
+import com.github.varenytsiamykhailo.packageTest.Vector;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+    }
+}
---
+++ examples/access/packageTest/src/com/github/varenytsiamykhailo/packageTest/Vector.java
@@ -0,0 +1,7 @@
+package com.github.varenytsiamykhailo.packageTest;
+
+public class Vector {
+    public Vector() {
+        System.out.println("myVector");
+    }
+}
