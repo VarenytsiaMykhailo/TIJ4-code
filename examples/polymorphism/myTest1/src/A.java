2df18360cfc9398bbdf70324e2373d35e6e69918
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2021-07-23 01:38:33 +0300

    1

---
+++ examples/polymorphism/myTest1/src/A.java
@@ -0,0 +1,10 @@
+public class A {
+    void f1() {
+        System.out.println("A.f1");
+        f2();
+    }
+
+    void f2() {
+        System.out.println("A.f2");
+    }
+}
---
+++ examples/polymorphism/myTest1/src/B.java
@@ -0,0 +1,6 @@
+public class B extends A {
+    @Override
+    void f2() {
+        System.out.println("B.f2");
+    }
+}
