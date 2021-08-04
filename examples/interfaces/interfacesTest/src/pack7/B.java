425cab6ffd488d44c64b999c6708c7ddec1bc26d
VarenytsiaMykhailo <52725148+VarenytsiaMykhailo@users.noreply.github.com>
2021-08-04 23:12:39 +0300

    1

---
+++ examples/interfaces/interfacesTest/src/pack7/B.java
@@ -0,0 +1,8 @@
+package pack7;
+
+public class B implements A {
+    //static int a = 11;
+    //@Override // @Override' not applicable to field
+    int a = 11;
+
+}
---
+++ examples/interfaces/interfacesTest/src/pack7/C.java
@@ -0,0 +1,5 @@
+package pack7;
+
+public interface C extends A {
+    int a = 15;
+}
